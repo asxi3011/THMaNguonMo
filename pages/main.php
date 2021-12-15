2773f870d8b9b5bec2b35f8aaed558155e1413a0
asxi3011 <asxi3011@gmail.com>
2021-12-15 22:18:31 +0700

    Thêm : Trang Giao diện cảm ơn

--- pages/main.php
+++ pages/main.php
@@ -1,3 +1,18 @@
 <div id="main">
-   <?php include("main/index.php") ?>;
+            <div class="maincontent">
+                  <?php
+               if(isset($_GET['quanly'])){
+                  $tam=$_GET['quanly'];
+               }else{
+                  $tam='';
+               }
+               if($tam=='camon'){
+                  include("main/camon.php");
+               }
+               else{
+                  include("main/index.php");
+               }
+
+               ?>
+			</div>
 </div>
