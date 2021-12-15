b46edfde37661697ead57129feff35294c658f02
asxi3011 <asxi3011@gmail.com>
2021-12-15 22:11:10 +0700

    Cập nhật: Layout cho index.php

--- index.php
+++ index.php
@@ -13,7 +13,12 @@
 </head>
 <body>
 	<div class="wrapper">
-		
+		<?php
+			include("pages/header.php");
+			include("pages/menu.php");
+			include("pages/main.php");
+			include("pages/footer.php");
+		?>
 	</div>
 </body>
 </html>
