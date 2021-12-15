feea3e7c2e3c1298a931731e9462c1afa5b8c102
asxi3011 <asxi3011@gmail.com>
2021-12-15 23:37:02 +0700

    Thêm: Chức năng xem đơn hàng

--- admincp/modules/main.php
+++ admincp/modules/main.php
@@ -3,9 +3,19 @@
 
 	<?php
 
-			
-        include("modules/dashboard.php");
-				
+			if(isset($_GET['action']) && $_GET['query']){
+				$tam = $_GET['action'];
+				$query = $_GET['query'];
+			}else{
+				$tam = '';
+				$query = '';
+			}
+			if($tam=='donhang' && $query=='xemdonhang'){
+				include("modules/quanlydonhang/xemdonhang.php");
+			}
+			else{
+				include("modules/dashboard.php");
+			}
 	?> 
 	
 </div>
